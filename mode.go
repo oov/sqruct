73eb7a9743d8c97793fce66e0904f929f632c84b
oov <ilovevipper@gmail.com>
2015-11-24 04:49:20 +0900

    add comments

--- mode.go
+++ mode.go
@@ -7,11 +7,17 @@ import (
 
 // Mode represents Sqruct processing mode.
 type Mode interface {
+	// String implemenets the Stringer interface.
 	String() string
+	// DefaultValueKeyword returns default value keyword that is used in insert statements.
 	DefaultValueKeyword() string
+	// IsAutoIncrement reports whether this column has auto increment constraint.
 	IsAutoIncrement(col string) bool
+	// Insert executes insert statement on db.
 	Insert(db DB, table string, columns []string, values []interface{}, autoIncrColumn int) (int64, error)
+	// PlaceholderGenerator creates placeholder generator that is used in SQL statements.
 	PlaceholderGenerator() PlaceholderGenerator
+	// Rebind replaces from '?' to default placeholder.
 	Rebind(string) string
 }
 
@@ -23,17 +29,14 @@ var (
 
 type mySQL struct{}
 
-// String implemenets the Stringer interface.
 func (mySQL) String() string { return "MySQL" }
 
 func (mySQL) DefaultValueKeyword() string { return "DEFAULT" }
 
-// IsAutoIncrement reports whether this column has auto increment constraint.
 func (mySQL) IsAutoIncrement(col string) bool {
 	return regexp.MustCompile(`(?i)\S+\s+AUTO_INCREMENT`).MatchString(col)
 }
 
-// Insert executes insert statement on db.
 func (m mySQL) Insert(db DB, table string, columns []string, values []interface{}, autoIncrColumn int) (int64, error) {
 	return genericInsert(db, table, columns, values, autoIncrColumn, m.DefaultValueKeyword(), m.PlaceholderGenerator())
 }
@@ -46,17 +49,14 @@ func (mySQL) Rebind(s string) string { return s }
 
 type postgreSQL struct{}
 
-// String implemenets the Stringer interface.
 func (postgreSQL) String() string { return "PostgreSQL" }
 
 func (postgreSQL) DefaultValueKeyword() string { return "DEFAULT" }
 
-// IsAutoIncrement reports whether this column has auto increment constraint.
 func (postgreSQL) IsAutoIncrement(col string) bool {
 	return regexp.MustCompile(`(?i)(?:BIG)?SERIAL|\S+\s+DEFAULT\s+nextval`).MatchString(col)
 }
 
-// Insert executes insert statement on db.
 func (m postgreSQL) Insert(db DB, table string, columns []string, values []interface{}, autoIncrColumn int) (int64, error) {
 	return postgresInsert(db, table, columns, values, autoIncrColumn, m.DefaultValueKeyword(), m.PlaceholderGenerator())
 }
@@ -85,17 +85,14 @@ func (m postgreSQL) Rebind(s string) string {
 
 type sqlite struct{}
 
-// String implemenets the Stringer interface.
 func (sqlite) String() string { return "SQLite" }
 
 func (sqlite) DefaultValueKeyword() string { return "NULL" }
 
-// IsAutoIncrement reports whether this column has auto increment constraint.
 func (sqlite) IsAutoIncrement(col string) bool {
 	return regexp.MustCompile(`(?i)\S+\s+AUTO_?INCREMENT|INTEGER\s+PRIMARY\s+KEY`).MatchString(col)
 }
 
-// Insert executes insert statement on db.
 func (m sqlite) Insert(db DB, table string, columns []string, values []interface{}, autoIncrColumn int) (int64, error) {
 	return genericInsert(db, table, columns, values, autoIncrColumn, m.DefaultValueKeyword(), m.PlaceholderGenerator())
 }
