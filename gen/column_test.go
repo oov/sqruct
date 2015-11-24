827c46722a564cfd519ba0f6b35fbba67455e22c
oov <ilovevipper@gmail.com>
2015-11-24 17:09:00 +0900

    fix broken test

--- gen/column_test.go
+++ gen/column_test.go
@@ -1,6 +1,10 @@
 package gen
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/oov/sqruct"
+)
 
 func TestColumnNotNull(t *testing.T) {
 	tests := []struct {
@@ -40,26 +44,26 @@ func TestColumnAutoIncrement(t *testing.T) {
 	tests := []struct {
 		c string
 		o bool
-		m Mode
+		m sqruct.Mode
 	}{
-		{c: "INTEGER PRIMARY KEY AUTO_INCREMENT", o: true, m: MySQL},
-		{c: "INTEGER PRIMARY KEY AUTOINCREMENT", o: false, m: MySQL},
-		{c: "INTEGER PRIMARY KEY", o: false, m: MySQL},
-		{c: "SERIAL", o: false, m: MySQL},
-		{c: "BIGSERIAL", o: false, m: MySQL},
+		{c: "INTEGER PRIMARY KEY AUTO_INCREMENT", o: true, m: sqruct.MySQL},
+		{c: "INTEGER PRIMARY KEY AUTOINCREMENT", o: false, m: sqruct.MySQL},
+		{c: "INTEGER PRIMARY KEY", o: false, m: sqruct.MySQL},
+		{c: "SERIAL", o: false, m: sqruct.MySQL},
+		{c: "BIGSERIAL", o: false, m: sqruct.MySQL},
 
-		{c: "INTEGER PRIMARY KEY AUTO_INCREMENT", o: false, m: PostgreSQL},
-		{c: "INTEGER PRIMARY KEY AUTOINCREMENT", o: false, m: PostgreSQL},
-		{c: "INTEGER PRIMARY KEY", o: false, m: PostgreSQL},
-		{c: "SERIAL", o: true, m: PostgreSQL},
-		{c: "BIGSERIAL", o: true, m: PostgreSQL},
+		{c: "INTEGER PRIMARY KEY AUTO_INCREMENT", o: false, m: sqruct.PostgreSQL},
+		{c: "INTEGER PRIMARY KEY AUTOINCREMENT", o: false, m: sqruct.PostgreSQL},
+		{c: "INTEGER PRIMARY KEY", o: false, m: sqruct.PostgreSQL},
+		{c: "SERIAL", o: true, m: sqruct.PostgreSQL},
+		{c: "BIGSERIAL", o: true, m: sqruct.PostgreSQL},
 
-		{c: "INTEGER PRIMARY KEY AUTO_INCREMENT", o: true, m: SQLite},
-		{c: "INTEGER PRIMARY KEY AUTOINCREMENT", o: true, m: SQLite},
-		{c: "INTEGER PRIMARY KEY", o: true, m: SQLite},
-		{c: "INT PRIMARY KEY", o: false, m: SQLite},
-		{c: "SERIAL", o: false, m: SQLite},
-		{c: "BIGSERIAL", o: false, m: SQLite},
+		{c: "INTEGER PRIMARY KEY AUTO_INCREMENT", o: true, m: sqruct.SQLite},
+		{c: "INTEGER PRIMARY KEY AUTOINCREMENT", o: true, m: sqruct.SQLite},
+		{c: "INTEGER PRIMARY KEY", o: true, m: sqruct.SQLite},
+		{c: "INT PRIMARY KEY", o: false, m: sqruct.SQLite},
+		{c: "SERIAL", o: false, m: sqruct.SQLite},
+		{c: "BIGSERIAL", o: false, m: sqruct.SQLite},
 	}
 	for i, v := range tests {
 		if r := (&Column{parent: &Table{parent: &Sqruct{Config: Config{Mode: v.m}}}, SQLColumn: v.c}).AutoIncrement(); r != v.o {
@@ -72,32 +76,32 @@ func TestColumnDefault(t *testing.T) {
 	tests := []struct {
 		c string
 		o bool
-		m Mode
+		m sqruct.Mode
 	}{
-		{c: "INTEGER PRIMARY KEY AUTO_INCREMENT", o: true, m: MySQL},
-		{c: "INTEGER PRIMARY KEY AUTOINCREMENT", o: false, m: MySQL},
-		{c: "INTEGER PRIMARY KEY", o: false, m: MySQL},
-		{c: "SERIAL", o: false, m: MySQL},
-		{c: "BIGSERIAL", o: false, m: MySQL},
-		{c: "INTEGER NOT NULL", o: false, m: MySQL},
-		{c: "INTEGER NOT NULL DEFAULT 0", o: true, m: MySQL},
+		{c: "INTEGER PRIMARY KEY AUTO_INCREMENT", o: true, m: sqruct.MySQL},
+		{c: "INTEGER PRIMARY KEY AUTOINCREMENT", o: false, m: sqruct.MySQL},
+		{c: "INTEGER PRIMARY KEY", o: false, m: sqruct.MySQL},
+		{c: "SERIAL", o: false, m: sqruct.MySQL},
+		{c: "BIGSERIAL", o: false, m: sqruct.MySQL},
+		{c: "INTEGER NOT NULL", o: false, m: sqruct.MySQL},
+		{c: "INTEGER NOT NULL DEFAULT 0", o: true, m: sqruct.MySQL},
 
-		{c: "INTEGER PRIMARY KEY AUTO_INCREMENT", o: false, m: PostgreSQL},
-		{c: "INTEGER PRIMARY KEY AUTOINCREMENT", o: false, m: PostgreSQL},
-		{c: "INTEGER PRIMARY KEY", o: false, m: PostgreSQL},
-		{c: "SERIAL", o: true, m: PostgreSQL},
-		{c: "BIGSERIAL", o: true, m: PostgreSQL},
-		{c: "INTEGER NOT NULL", o: false, m: PostgreSQL},
-		{c: "INTEGER NOT NULL DEFAULT 0", o: true, m: PostgreSQL},
+		{c: "INTEGER PRIMARY KEY AUTO_INCREMENT", o: false, m: sqruct.PostgreSQL},
+		{c: "INTEGER PRIMARY KEY AUTOINCREMENT", o: false, m: sqruct.PostgreSQL},
+		{c: "INTEGER PRIMARY KEY", o: false, m: sqruct.PostgreSQL},
+		{c: "SERIAL", o: true, m: sqruct.PostgreSQL},
+		{c: "BIGSERIAL", o: true, m: sqruct.PostgreSQL},
+		{c: "INTEGER NOT NULL", o: false, m: sqruct.PostgreSQL},
+		{c: "INTEGER NOT NULL DEFAULT 0", o: true, m: sqruct.PostgreSQL},
 
-		{c: "INTEGER PRIMARY KEY AUTO_INCREMENT", o: true, m: SQLite},
-		{c: "INTEGER PRIMARY KEY AUTOINCREMENT", o: true, m: SQLite},
-		{c: "INTEGER PRIMARY KEY", o: true, m: SQLite},
-		{c: "INT PRIMARY KEY", o: false, m: SQLite},
-		{c: "SERIAL", o: false, m: SQLite},
-		{c: "BIGSERIAL", o: false, m: SQLite},
-		{c: "INTEGER NOT NULL", o: false, m: SQLite},
-		{c: "INTEGER NOT NULL DEFAULT 0", o: true, m: SQLite},
+		{c: "INTEGER PRIMARY KEY AUTO_INCREMENT", o: true, m: sqruct.SQLite},
+		{c: "INTEGER PRIMARY KEY AUTOINCREMENT", o: true, m: sqruct.SQLite},
+		{c: "INTEGER PRIMARY KEY", o: true, m: sqruct.SQLite},
+		{c: "INT PRIMARY KEY", o: false, m: sqruct.SQLite},
+		{c: "SERIAL", o: false, m: sqruct.SQLite},
+		{c: "BIGSERIAL", o: false, m: sqruct.SQLite},
+		{c: "INTEGER NOT NULL", o: false, m: sqruct.SQLite},
+		{c: "INTEGER NOT NULL DEFAULT 0", o: true, m: sqruct.SQLite},
 	}
 	for i, v := range tests {
 		if r := (&Column{parent: &Table{parent: &Sqruct{Config: Config{Mode: v.m}}}, SQLColumn: v.c}).Default(); r != v.o {
